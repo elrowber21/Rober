ee8ed623df596e43c75edac182d3054c7bf21014
elrowber21 <60397994+elrowber21@users.noreply.github.com>
2020-02-12 12:46:25 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Rober
+{
+    class Rober
+    {
+        private string nombre;
+        private int edad;
+        private string correo;
+    
+        public string getNombre(){
+            return nombre;
+        }
+        public void setNombre(string n)
+        {
+            nombre =n;
+        }
+         public int getEdad()
+        {
+            return edad;
+        }public void setEdad(int e)
+        {
+            edad = e;
+        }
+       
+        public string getCorreo()
+        {
+            return correo;
+        }
+        public void setCorreo(string c)
+        {
+           correo=c;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rober p= new Rober();
+            p.setNombre("Ana");
+            p.setEdad(18);
+            p.setCorreo("maquinitadeamor@yimeil.com");
+            
+            Rober p2=new Rober();
+            p2.setEdad(787);
+            p2.setNombre("Johnny");
+            p2.setCorreo("otramaquinita@jaifaib.com");
+
+            Console.WriteLine(p.getNombre());
+            Console.WriteLine(p2.getNombre());
+            Console.WriteLine(p.getCorreo());
+            Console.WriteLine(p.getEdad());
+            Console.WriteLine(p2.getCorreo());
+            Console.WriteLine(p2.getEdad());
+        }
+    }
+}
